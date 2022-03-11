a0f4e618ef1b011c20931f8f4591a05ec6e6188d
MaxiT <maxitroyt@gmail.com>
2022-03-11 00:04:39 -0300

    First commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "../views/Detail";
+import Home from "../views/Home";
+import Results from "../views/Results";
+
+const RoutesComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RoutesComponent;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    fontFamily: {
+      lato: ["lato", "sans-serif"],
+    },
+    colors: {
+      "special-gray": "#F5F0F0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
