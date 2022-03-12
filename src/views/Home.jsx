67d191b2a1d6cf3791bc6e8e4d7146a6e7791e35
MaxiT <maxitroyt@gmail.com>
2022-03-11 22:31:54 -0300

    Add styled and functionalities

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Chair from "../assets/chair.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState("");
+
+  const handleInputSearch = ({ target: { value } }) => {
+    setSearch(value);
+  };
+
+  const handleClean = () => {
+    setSearch("");
+  };
+
+  const handleSearchClick = () => {
+    navigate(`/results/${search.trim()}`);
+  };
+
   return (
     <div className="flex h-screen overflow-hidden">
       <div className="w-2/5">
@@ -13,10 +29,29 @@ const Home = () => {
       </div>
       <div className="w-3/5 flex justify-center items-center flex-col px-10">
         <h2 className="text-4xl font-bold font-lato">Busca tu Pelicula...</h2>
-        <input className="bg-special-gray font-lato w-full my-3 h-8 p-1" />
-        <div>
-          <button>Buscar</button>
-          <button>Limpiar</button>
+        <input
+          className="bg-special-gray font-lato w-full my-3 h-8 p-1 focus:outline-none focus:ring-2 
+          focus:ring-special-gray-600 rounded"
+          value={search}
+          onChange={handleInputSearch}
+        />
+        <div className="flex w-full justify-between">
+          <button
+            className="bg-special-red hover:bg-special-red-800 text-white font-lato w-full shadow-lg 
+              h-9"
+            style={{ width: "48%" }}
+            onClick={handleSearchClick}
+          >
+            Buscar
+          </button>
+          <button
+            className="bg-special-red hover:bg-special-red-800 text-white font-lato w-full shadow-lg 
+              h-9"
+            style={{ width: "48%" }}
+            onClick={handleClean}
+          >
+            Limpiar
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
@@ -5,6 +7,11 @@ module.exports = {
       lato: ["lato", "sans-serif"],
     },
     colors: {
+      ...defaultTheme.colors,
+      white: "#fff",
+      "special-gray-600": "#4b5563",
+      "special-red": "#DC2626",
+      "special-red-800": "#991B1B",
       "special-gray": "#F5F0F0",
     },
     extend: {},
