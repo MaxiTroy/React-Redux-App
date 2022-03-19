45e09af843894e3d78fa40693467ccb70ffba6c1
MaxiT <maxitroyt@gmail.com>
2022-03-19 19:55:29 -0300

    Separate into components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,27 +1,14 @@
 import React from "react";
+import ListItem from "./ListItem";
 
 const List = ({ data }) => {
   return data?.map((item) => (
-    <div
+    <ListItem
       key={item.imdbID}
-      className="flex flex-row w-full mt-16"
-      style={{ minWidth: 650 }}
-    >
-      <div className="w-1/5">
-        <img src={item?.Poster} alt={item.Title} className="w-32" />
-      </div>
-      <div className="w-4/5 flex flex-col items-start py-2 px-3">
-        <p className="font-lato text-3xl">{item.Title}</p>
-        <div className="flex h-full items-end">
-          <button
-            className="border-2 border-special-red rounded-md w-48 h-10 
-              text-xl text-special-red hover:bg-special-red hover:text-white"
-          >
-            Ver Mas
-          </button>
-        </div>
-      </div>
-    </div>
+      imdbID={item?.imdbID}
+      Poster={item?.Poster}
+      Title={item?.Title}
+    />
   ));
 };
 
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ListItem = ({ imdbID, Poster, Title }) => {
+  return (
+    <div
+      key={imdbID}
+      className="flex flex-row w-full mt-16"
+      style={{ minWidth: 650 }}
+    >
+      <div className="w-1/5">
+        <img src={Poster} alt={Title} className="w-32" />
+      </div>
+      <div className="w-4/5 flex flex-col items-start py-2 px-3">
+        <p className="font-lato text-3xl">{Title}</p>
+        <div className="flex h-full items-end">
+          <button
+            className="border-2 border-special-red rounded-md w-48 h-10 
+          text-xl text-special-red hover:bg-special-red hover:text-white"
+          >
+            Ver Mas
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
